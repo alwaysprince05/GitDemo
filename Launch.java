4dd9bc9e4b5b381aef04677efc1ec0ba0f126a64
alwaysprince05 <prince.p24@medhaviskillsuniversity.edu.in>
2025-07-01 13:27:48 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -3,4 +3,11 @@ public class Launch {
     public static void main(String[] args) {
         System.out.println("Git & Github");
     }
+
+    public void fun(){
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println(sum);
+    }
 }
